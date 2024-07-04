78b22476ed177e37e939880fae545bfc768621e4
Nooralrihany <Nooralrihany@gmail.com>
2024-07-04 14:02:19 +0300

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Navbar } from './components/Navbar';
+import { Sidebar } from './Sidebar';
 // import { Auth} from './components/auth'
 
 const App = () => {
@@ -7,6 +8,7 @@ const App = () => {
     <>
     <Navbar/>
     {/* <Auth/> */}
+    <Sidebar/>
     </>
   )
 }
