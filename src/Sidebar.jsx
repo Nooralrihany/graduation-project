5c290b9f6e96b41e1522c6e0dd4a6de6962b4b3b
Nooralrihany <Nooralrihany@gmail.com>
2024-07-17 19:48:38 +0300

    fixed the image

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -4,17 +4,17 @@ import homephoto from "../src/assets/homephoto.jpg"
 
 export const Sidebar = () => {
     return (
-      <div>
-        <img src="src/assets/homephoto.jpg" alt="home" className=' bg-center' />
-        <div className="bg-gray-100 min-h-screen px-8 py-16" >
+      <div   className=' relative'>
+        <img src="src/assets/homephoto.jpg" alt="home" className=' bg-center w-full' />
+
           
-          <div className='border-2 border-red-400 max-w-xl rounded-lg'>
+          <div className='border-2  absolute top-1/2 bg-white  max-w-xl rounded-lg '>
            {/* <img src="src/assets/homephoto.jpg" alt="home" className='bg-cover	' /> */}
            <div className="max-w-2xl p-8 bg-blue shadow-lg rounded-lg">
             <h1 className="text-3xl font-bold text-gray-800 mb-4 uppercase">Welcome to our community</h1>
             <h4 className="text-xl mb-2">Park your car with us !</h4>
             <p className="text-gray-600">تطبيق يساعدك في الحصول على مكان لركن سيارتك ب أمان</p>
-           </div>
+      
           </div>
         </div>
       </div>
